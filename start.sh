191ffa9d547d6f8ae2afaefee5d348302085b4a0
Liam <lmacka@gmail.com>
2024-08-14 10:45:48 +1000

    no need to stash then pop

--- start.sh
+++ start.sh
@@ -6,14 +6,8 @@ BRANCH=devel
 # Navigate to the project directory
 cd /home/$USER/chicky
 
-# Stash local changes to config.js and start.sh
-git stash push -m "Stash before pull" config.js start.sh
-
 # Pull the latest code from the repository
 git pull origin $BRANCH
-
-# Apply the stashed changes back
-git stash pop
 
 # Start the Node.js application
 /usr/bin/node /home/$USER/chicky/server.js
